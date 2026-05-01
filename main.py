dde9ea0c9307faade0c3f6c76741fdae42219323
zack06g <zack05g@gmail.com>
2026-05-01 12:29:10 -0400

    Set up main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+#region Imports
+#endregion
+
+#region GUI
+#endregion
+
+#region Music Player
+#endregion
+
+#region File Writer
+#endregion
+
+#region Weather App
+#endregion
+
+#region Web Scrapper
+#endregion
+
+#region Settings
+#endregion
+
+#region Runner
+#endregion
